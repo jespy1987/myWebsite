d2c003b1f180ed7d98e5f2bf2569fe664a255780
Oksana Klymenok <kapris15@yahoo.com>
2020-05-07 00:44:09 -1000

    still working on Cart

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,80 @@
+
+function save_data(id_amount, id_price, id_div, name_activity) {
+  var input = document.getElementById(id_amount);
+  localStorage.setItem("people", input.value);
+  var storedValue1 = localStorage.getItem("people");
+  addElement(storedValue1, id_price, id_div, name_activity);
+}
+
+$("#btnSailing").click(function(){
+  save_data("peopleSa", "priceSa", "1", "Sailing");
+});
+$("#btnSnorkeling").click(function(){
+  save_data("peopleSn", "priceSn", "2", "Snorkeling");
+});
+$("#btnSurfing").click(function(){
+  save_data("peopleSu", "priceSu", "3", "Surfing")
+});
+
+
+
+function sub_total(){
+
+}
+
+$("#Cart").click(function () {
+  sub_total();
+  $("#cartModal").modal("show");
+});
+
+
+function addElement(addElem, id_price, id_div, name_activity) {
+
+  if ((addElem === "") || (addElem === "0")) {
+    return 0;
+  }
+  if (document.getElementById("div"+id_div)){
+    let div_del=document.getElementById("div"+id_div);
+    div_del.parentNode.removeChild(div_del);
+  }
+var div=document.createElement("div");
+div.setAttribute("id","div"+id_div);
+div.innerHTML="<h2 id='header"+id_div+"'></h2><label for='price"+id_div+"'>Price</label><input type='text' disabled id='price"+id_div+"'><br><label for='amountOf"+id_div+"'>Amount of people</label><input type='text' disabled id='amountOf"+id_div+"'>";
+var parent=document.getElementById("test");
+parent.appendChild(div);
+var set_amount=document.getElementById("amountOf"+id_div);
+set_amount.value=addElem;
+var act_price=document.getElementById(id_price);
+var act_price_modal=document.getElementById("price"+id_div);
+act_price_modal.value=act_price.value;
+var headerName=document.getElementById("header"+id_div);
+headerName.innerHTML=name_activity;
+var a=parseInt(addElem);
+console.log(a);
+var price_string=act_price.value;
+console.log(price_string);
+var b=parseInt(price_string);
+console.log(b);
+var total=a*b;
+console.log(total);
+}
+
+
+
+
+
+
+// var copy_div=document.getElementById("div");
+// var the_clone=copy_div.cloneNode(true);
+// the_clone.setAttribute('class', 'd-block');
+// var parent=document.getElementById("cartForm");
+// parent.appendChild(the_clone);
+// document.getElementById("")
+
+
+
+
+
 // $(document).ready(function () {
 //   autoPlayYouTubeModal();
 // });
@@ -14,23 +91,45 @@
 //     });
 //   });
 // }
-function addElement(){
-  var newElement=document.createElement("input");
-  newElement.setAttribute('type', 'text');
-  var parent=document.getElementById("test").appendChild(newElement);
+// function addElement(){
+//   var newElement=document.createElement("input");
+//   newElement.setAttribute('type', 'text');
+//   var storedValue1 = localStorage.getItem("peopleS");
+//   newElement.setAttribute('value',storedValue1);
+//   var parent=document.getElementById("test").appendChild(newElement);
 
-}
-function save_data1() {
-  var input = document.getElementById("peopleS");
-  localStorage.setItem("peopleS", input.value);
-  var storedValue1 = localStorage.getItem("peopleS");
-    function addElement();  
-}
+// }
 
 
 
+// function addElement(storedValue) {
+//   if ((storedValue === "") || (storedValue === "0")) {
+//     return;
+//   }
+// if (document.getElementById("storedValue")) {
+//   var delInput=document.getElementById("storedValue");
+//   delInput.parentNode.removeChild(delInput);
+//   var delLabel=document.getElementById("")
+// }
+
+//   var newElement = document.createElement("input");
+//   newElement.setAttribute('type', 'text');
+//   newElement.setAttribute('value', storedValue);
+//   newElement.setAttribute('id', 'storedValue')
+//   var newLabel = document.createElement("label");
+//   newLabel.setAttribute('for', 'storedValue');
+//   newLabel.innerHTML = "Amount of people";
+//   var parent = document.getElementById("test");
+//   parent.appendChild(newLabel);
+//   parent.appendChild(newElement);
+//   var newPrice = document.getElementById("priceS");
+//   var the_clone = newPrice.cloneNode(true);
+//   the_clone.setAttribute('id', 'price2');
+//   var newLabel2 = document.createElement("label");
+//   newLabel2.setAttribute('for', 'price2');
+//   newLabel2.innerHTML = "Price";
+//   parent.appendChild(newLabel2);
+//   parent.appendChild(the_clone);
 
 
-$("#Cart").click(function () {
-       $("#cartModal").modal("show");
-});
+// }
