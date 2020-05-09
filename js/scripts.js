4c2e659e3b7a226c9833cb71ec2aa4393e55b249
Oksana Klymenok <kapris15@yahoo.com>
2020-05-08 16:46:58 -1000

    Before visual changes

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,44 @@ function save_data(id_amount, id_price, id_div, name_activity) {
   addElement(storedValue1, id_price, id_div, name_activity);
 }
 
-$("#btnSailing").click(function(){
+$("#btnSailing").click(function () {
   save_data("peopleSa", "priceSa", "1", "Sailing");
 });
-$("#btnSnorkeling").click(function(){
+$("#btnSnorkeling").click(function () {
   save_data("peopleSn", "priceSn", "2", "Snorkeling");
 });
-$("#btnSurfing").click(function(){
+$("#btnSurfing").click(function () {
   save_data("peopleSu", "priceSu", "3", "Surfing")
 });
 
 
 
-function sub_total(){
+function sub_total() {
+  var array_total = [];
+  for (let i = 1; i < 4; i++) {
+    if (document.getElementById("div" + i)) {
+      let sub_total = document.getElementById("total" + i).value;
+      var sub_total_int = parseInt(sub_total);
+      array_total.push(sub_total_int);
+      console.log(array_total);
+    }
+  }
+  var subTotal = array_total.reduce(function (a, b) { return a + b }, 0);
+  
+  if (document.getElementById('divTotal')) {
+    let divTotalDel=document.getElementById('divTotal');
+    divTotalDel.parentNode.removeChild(divTotalDel);
+  }
+  if (subTotal === 0) { return; }
+  var parentTotal = document.getElementById("test");
+  var totalInput = document.createElement("div");
+  totalInput.setAttribute('id', 'divTotal');
+  totalInput.innerHTML = "<h1>Your Total Price</h1><br><label for='subTotal'>Your Total is: </label><input type='number' id='subTotal' disabled>";
+  parentTotal.appendChild(totalInput);
+  document.getElementById("subTotal").value = subTotal;
+
+
+
 
 }
 
@@ -31,36 +56,44 @@ $("#Cart").click(function () {
 function addElement(addElem, id_price, id_div, name_activity) {
 
   if ((addElem === "") || (addElem === "0")) {
+    if (document.getElementById("div" + id_div)) {
+      let div_del = document.getElementById("div" + id_div);
+      div_del.parentNode.removeChild(div_del);
+    }
+    
     return 0;
   }
-  if (document.getElementById("div"+id_div)){
-    let div_del=document.getElementById("div"+id_div);
+  if (document.getElementById("div" + id_div)) {
+    let div_del = document.getElementById("div" + id_div);
     div_del.parentNode.removeChild(div_del);
   }
-var div=document.createElement("div");
-div.setAttribute("id","div"+id_div);
-div.innerHTML="<h2 id='header"+id_div+"'></h2><label for='price"+id_div+"'>Price</label><input type='text' disabled id='price"+id_div+"'><br><label for='amountOf"+id_div+"'>Amount of people</label><input type='text' disabled id='amountOf"+id_div+"'>";
-var parent=document.getElementById("test");
-parent.appendChild(div);
-var set_amount=document.getElementById("amountOf"+id_div);
-set_amount.value=addElem;
-var act_price=document.getElementById(id_price);
-var act_price_modal=document.getElementById("price"+id_div);
-act_price_modal.value=act_price.value;
-var headerName=document.getElementById("header"+id_div);
-headerName.innerHTML=name_activity;
-var a=parseInt(addElem);
-console.log(a);
-var price_string=act_price.value;
-console.log(price_string);
-var b=parseInt(price_string);
-console.log(b);
-var total=a*b;
-console.log(total);
+  var div = document.createElement("div");
+  div.setAttribute("id", "div" + id_div);
+  div.innerHTML = "<h2 id='header" + id_div + "'></h2><label for='price" + id_div + "'>Price $</label><input type='text' disabled id='price" + id_div + "'><br><label for='amountOf" + id_div + "'>Amount of people</label><input type='text' disabled id='amountOf" + id_div + "'><br><label for='total" + id_div + "'>Total</label><input type='text' disabled id='total" + id_div + "'>";
+  var parent = document.getElementById("test");
+  parent.appendChild(div);
+  var set_amount = document.getElementById("amountOf" + id_div);
+  set_amount.value = addElem;
+  var act_price = document.getElementById(id_price);
+  var act_price_modal = document.getElementById("price" + id_div);
+  act_price_modal.value = act_price.value;
+  var headerName = document.getElementById("header" + id_div);
+  headerName.innerHTML = name_activity;
+
+  var price_string = act_price.value;
+  var totalPrice = total(addElem, price_string);
+  document.getElementById("total" + id_div).value = totalPrice;
+
 }
 
 
+function total(x, y) {
+  var a = parseInt(x);
+  var b = parseInt(y);
+  let total = a * b;
+  return total;
 
+}
 
 
 
